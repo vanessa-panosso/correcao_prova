c5dd3620df2958a15521062449db06e573d8eece
VLBPANOSSO <VLBPANOSSO@WS-LAB08_142.univel.univel.br>
2017-03-20 20:45:01 -0300

    Correcao

--- CorrrecaoProva/src/br/univel/cliente/Zoom.java
+++ CorrrecaoProva/src/br/univel/cliente/Zoom.java
@@ -3,6 +3,7 @@ package br.univel.cliente;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
+import br.univel.comum.CONSTANTES;
 import br.univel.comum.Plact;
 
 public class Zoom {
@@ -12,7 +13,7 @@ public class Zoom {
 	public Zoom() throws Exception {
 		Registry registry = LocateRegistry.getRegistry("127.0.0.1", 1818);
 		Plact servico = (Plact) registry.lookup(Plact.NOME);
-		Integer retorno = servico.calcule(43);
+		Integer retorno = servico.calcule(CONSTANTES.RA_DEZENA);
 	}
 	public static void main(String[] args) throws Exception {
 		new Zoom();
---
+++ CorrrecaoProva/src/br/univel/comum/CONSTANTES.java
@@ -0,0 +1,5 @@
+package br.univel.comum;
+
+public class CONSTANTES {
+	public static final Integer RA_DEZENA = 43;
+}
